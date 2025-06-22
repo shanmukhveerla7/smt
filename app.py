e6affc5959c20a7c7293a46f9a7a7f5381f49961
shanmukhveerla7 <rs200613@rguktsklm.ac.in>
2025-06-22 23:01:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ elif st.session_state.current_section == "profile":
         st.markdown('<br>', unsafe_allow_html=True)
         if st.button("🔄 Reset Profile"):
             reset_profile()
-    st.markdown('</div>')
+    st.markdown('Thank You!!')
 
 # If profile not completed, stop further access
 elif not st.session_state.profile_complete:
